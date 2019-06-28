aa724ae347cac45736ce44272117252dff080cf4
sudonym221 <deepjchetia007@gmail.com>
2019-06-28 09:07:58 +0530

    Email and Phone number validation is added

--- models/customer.js
+++ models/customer.js
@@ -1,10 +1,27 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const customerSchema = mongoose.Schema({
     firstname : {type : String},
     lastName : {type : String},
-    phone : {type : String},
-    email : {type : String}
+    phone : {
+        type : String,
+        validate:{
+            validator(value){
+                if(!validator.isMobilePhone(value, ['en-IN'])) throw new Error('Email not Valid');
+            }
+        }
+    },
+    
+    email : {
+        type : String,
+        validate:{
+            validator(value){
+                if(!validator.isEmail(value)) throw new Error('Email not Valid');
+            }
+        }
+    }
+    
 });
 
 // Export
